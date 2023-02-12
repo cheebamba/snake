53b92d43467e820785f61ebd0b57e3fee0c6a797
Cheebamba <kamil.cziba@gmail.com>
2023-02-12 01:21:41 +0100

    version 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { type FC, useState, useEffect, useRef } from "react";
+import { type FC, useState, useEffect } from "react";
 import useGameStateStore from "./store/main";
 
 function App() {
@@ -7,45 +7,135 @@ function App() {
     speed,
     snake,
     menu,
+    score,
+    food,
+    currentEaten,
     currentTick,
     setCurTick,
     increaseTick,
+    increaseScore,
+    increaseSpeed,
+    resetEaten,
+    increaseEaten,
+    getSnake,
     setInter,
     reset,
     toggleMenu,
+    addPosition,
+    addFood,
   } = useGameStateStore();
+
   // 1 - up, 2 - right, 3 - down, 4 - left
   const [direction, setDirection] = useState<1 | 2 | 3 | 4>(1);
+  const [showScore, setShowSore] = useState<boolean>(false);
 
   useEffect(() => {
     document.addEventListener("keydown", keyHandler);
   }, []);
 
-  useEffect(() => {}, [menu]);
-
   useEffect(() => {
-    if (currentTick >= 10 / speed) {
+    if (currentTick >= 100 / speed) {
       move();
       setCurTick(0);
     }
   }, [currentTick]);
 
+  function start() {
+    setDirection(1);
+    reset();
+    toggleMenu();
+    addFood();
+    addFood();
+    setInter(
+      setInterval(() => {
+        increaseTick();
+      }, 10)
+    );
+  }
+
+  function stop() {
+    clearInterval(interval);
+    setInter(0);
+  }
+
+  function end() {
+    toggleMenu();
+    stop();
+    setShowSore(true);
+  }
+
+  function checkBackwards(direction: 1 | 2 | 3 | 4) {
+    let updatedSnake = getSnake();
+    let x = updatedSnake[0][0];
+    let y = updatedSnake[0][1];
+    let sx = 99;
+    let sy = 99;
+    if (updatedSnake[1]) {
+      sx = updatedSnake[1][0];
+      sy = updatedSnake[1][1];
+    }
+    if (direction === 1 && y + 1 === sy) {
+      return true;
+    }
+    if (direction === 2 && x + 1 === sx) {
+      return true;
+    }
+    if (direction === 3 && y - 1 === sy) {
+      return true;
+    }
+    if (direction === 4 && x - 1 === sx) {
+      return true;
+    }
+    return false;
+  }
+
   function keyHandler(ev: KeyboardEvent) {
     if (ev.code === "KeyW") {
+      if (checkBackwards(1)) {
+        return;
+      }
       setDirection(1);
     }
     if (ev.code === "KeyD") {
+      if (checkBackwards(2)) {
+        return;
+      }
       setDirection(2);
     }
     if (ev.code === "KeyS") {
+      if (checkBackwards(3)) {
+        return;
+      }
       setDirection(3);
     }
     if (ev.code === "KeyA") {
+      if (checkBackwards(4)) {
+        return;
+      }
       setDirection(4);
     }
-    if (ev.code === "Escape") {
-      stop();
+  }
+
+  function makeMove(x: number, y: number) {
+    if (x < 0 || x > 20 || y < 0 || y > 20) {
+      end();
+      return null;
     }
+    if (snake.find((e) => e[0] === x && e[1] === y)) {
+      end();
+    }
+    let eaten = false;
+    if (food.find((e) => e[0] === x && e[1] === y)) {
+      eaten = true;
+      addFood();
+      increaseScore(1);
+      increaseEaten();
+      if (currentEaten > 2) {
+        resetEaten();
+        increaseSpeed();
+      }
+    }
+    addPosition(x, y, eaten);
   }
 
   function move() {
@@ -56,76 +146,47 @@ function App() {
 
     switch (direction) {
       case 1:
+        newX = curX;
         newY = curY + 1;
-        if (newY > 20) {
-          end();
-          return null;
-        }
-        snake.unshift([curX, newY]);
         break;
       case 2:
         newX = curX + 1;
-        if (newX > 20) {
-          end();
-          return null;
-        }
-        snake.unshift([newX, curY]);
+        newY = curY;
         break;
       case 3:
+        newX = curX;
         newY = curY - 1;
-        if (newY < 0) {
-          end();
-          return null;
-        }
-        snake.unshift([curX, newY]);
         break;
       default:
         newX = curX - 1;
-        if (newX < 0) {
-          end();
-          return null;
-        }
-        snake.unshift([newX, curY]);
+        newY = curY;
         break;
     }
-    snake.pop();
+    makeMove(newX, newY);
   }
 
-  function start() {
-    setDirection(1);
-    reset();
-    toggleMenu();
-    setInter(
-      setInterval(() => {
-        increaseTick();
-      }, 100)
-    );
-  }
-
-  function stop() {
-    clearInterval(interval);
-    setInter(0);
-  }
-
-  const Snake: FC = () => {
-    let cords = snake[0];
-    let x = cords[0];
-    let y = cords[1];
+  const Snake: FC<{ x: number; y: number }> = ({ x, y }) => {
     let left = x * 20;
     let bot = y * 20;
 
     return (
       <div
         style={{ left: left, bottom: bot }}
-        className={`absolute h-[20px] w-[20px] bg-slate-50`}
+        className={`absolute z-10 h-[20px]  w-[20px] bg-slate-50`}
       ></div>
     );
   };
 
-  function end() {
-    toggleMenu();
-    stop();
-  }
+  const Food: FC<{ x: number; y: number }> = ({ x, y }) => {
+    let left = x * 20;
+    let bot = y * 20;
+    return (
+      <div
+        style={{ left: left, bottom: bot }}
+        className={`absolute z-0 h-[20px] w-[20px] bg-green-500`}
+      ></div>
+    );
+  };
 
   const Menu: FC<{
     className?: string;
@@ -133,13 +194,20 @@ function App() {
   }> = ({ children, className }) => {
     return (
       <div
-        className={`absolute h-full w-full bg-gray-800 bg-opacity-95 ${className}`}
+        className={`absolute z-20 h-full w-full bg-gray-800 bg-opacity-95 ${className}`}
       >
         <div className="flex h-full w-full flex-col items-center justify-center gap-3 align-middle">
           {children && children}
         </div>
       </div>
     );
+  };
+
+  const Score: FC = () => {
+    if (showScore) {
+      return <div className="text-xl text-gray-200">Score: {score}</div>;
+    }
+    return null;
   };
 
   const Button: FC<{ title: string; action?: () => void }> = ({
@@ -159,9 +227,18 @@ function App() {
   return (
     <div className="flex h-full w-full items-center justify-center align-middle">
       <div className="relative h-[420px] w-[420px] bg-slate-900">
-        <Snake />
+        {snake &&
+          snake.map((el, key) => {
+            return <Snake key={key} x={el[0]} y={el[1]} />;
+          })}
+
+        {food &&
+          food.map((el, key) => {
+            return <Food key={key} x={el[0]} y={el[1]} />;
+          })}
         {menu && (
           <Menu>
+            <Score />
             <Button title="Start" action={start} />
           </Menu>
         )}
--- src/store/main.ts
+++ src/store/main.ts
@@ -4,45 +4,98 @@ interface GameState {
   speed: number;
   currentTick: number;
   interval: number;
+  currentEaten: number;
+  score: number;
   snake: number[][];
+  food: number[][];
   menu: boolean;
   reset: () => void;
   toggleMenu: () => void;
   setCurTick: (ref: number) => void;
   increaseTick: () => void;
   setInter: (ref: number) => void;
-  increaseSpeed: (by: number) => void;
+  increaseSpeed: () => void;
+  increaseEaten: () => void;
+  getSnake: () => number[][];
+  resetEaten: () => void;
+  increaseScore: (by: number) => void;
+  addPosition: (x: number, y: number, eaten: boolean) => void;
+  addFood: () => void;
 }
 
 const initState = {
   currentTick: 0,
-  // speed
-  speed: 3,
-  // interval ref
+  speed: 4,
   interval: 0,
+  currentEaten: 0,
+  score: 0,
   menu: true,
-  // starting snake
   snake: [[10, 2]],
+  food: [],
 };
 
-const useGameStateStore = create<GameState>()((set) => ({
+const useGameStateStore = create<GameState>()((set, get) => ({
   ...initState,
-  reset: () =>
-    set((state) => ({
-      currentTick: 0,
-      // speed
-      speed: 3,
-      // interval ref
-      interval: 0,
-      menu: true,
-      // starting snake
-      snake: [[10, 2]],
-    })),
+  reset: () => set((state) => ({ ...initState })),
   toggleMenu: () => set((state) => ({ menu: !state.menu })),
-  setCurTick: (tick: number) => set((state) => ({ currentTick: tick })),
+  setCurTick: (tick) => set((state) => ({ currentTick: tick })),
   increaseTick: () => set((state) => ({ currentTick: state.currentTick + 1 })),
-  setInter: (ref: number) => set((state) => ({ interval: ref })),
-  increaseSpeed: (by: number) => set((state) => ({ speed: state.speed + by })),
+  setInter: (ref) => set((state) => ({ interval: ref })),
+  getSnake: () => {
+    const snake: number[][] = get().snake;
+    return snake;
+  },
+  increaseSpeed: () =>
+    set((state) => {
+      return { speed: state.speed + 1 };
+    }),
+  increaseEaten: () =>
+    set((state) => ({ currentEaten: state.currentEaten + 1 })),
+  resetEaten: () => set(() => ({ currentEaten: 0 })),
+  increaseScore: (by: number) => set((state) => ({ score: state.score + by })),
+  addPosition: (x, y, eaten) => {
+    set((state) => {
+      const [tail, ...rest] = state.snake.reverse();
+      let restSnake = rest.reverse();
+      if (eaten) {
+        state.food.splice(
+          state.food.findIndex((e) => e[0] === x && e[1] === y),
+          1
+        );
+        restSnake = [...rest, tail];
+      }
+
+      return {
+        ...state,
+        snake: [[x, y], ...restSnake],
+      };
+    });
+  },
+  addFood: () => {
+    set((state) => {
+      let x = 0;
+      let y = 0;
+      function randPos() {
+        return Math.floor(Math.random() * 20);
+      }
+      function checkIfValid(x: number, y: number) {
+        return !state.snake.find((e) => e[0] === x && e[1] === y);
+      }
+      function assignNumbers() {
+        x = randPos();
+        y = randPos();
+        if (!checkIfValid(x, y)) {
+          assignNumbers();
+        }
+      }
+      assignNumbers();
+
+      return {
+        ...state,
+        food: [[x, y], ...state.food],
+      };
+    });
+  },
 }));
 
 export default useGameStateStore;
